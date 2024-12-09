fd6edba0332fa19ba9d252196c24b5a8cd648945
dilshan anupriya <dilshananupriya@icloud.com>
2024-12-09 21:24:38 +0530

    customer crud

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ class user{
         this.password =password;
     }
 }
-class customer{
+class Customer{
     constructor(name,address,salary,nic){
         this.name=name;
         this.address = address;
@@ -61,11 +61,60 @@ const login =()=>{
     }
 }
 
-const createCustomer=()=>{}
-const findCustomer=(id)=>{}
-const updateCustomer=(id)=>{}
-const deleteCustomer=(id)=>{}
-const getAllCustomer=()=>{}
+const createCustomer=()=>{
+    const customer = new Customer(
+        $('#name').val(),
+        $('#address').val(),
+        parseFloat($('#salary').val()),
+        $('#nic').val()
+    )
+    let exist = cusArr.find(e=>e.nic ===Customer.nic);
+    if(exist){
+        alert('nic already exist')
+        return;
+    }
+    cusArr.push(customer);
+}
+const findCustomer=(id)=>{
+    let selectedCustomer = cusArr.find(e=>e.nic === Customer.nic );
+    if(selectedCustomer){
+        return selectedCustomer;
+    }
+    return null;
+}
+const updateCustomer=(id)=>{
+let customer = findCustomer(id);
+if(customer){
+    $('#name').val(Customer.name),
+    $('#address').val(Customer.address),
+    $('#salary').val(Customer.salary),
+    $('#nic').val(Customer.nic)
+}else{
+    alert('Customer not found');
+}
+}
+const deleteCustomer=(id)=>{
+    for(let i = 0; i < cusArr.length; i++){
+        if(cusArr[i].nic === id){
+            cusArr.splice(i,1);
+            alert('deleted');
+            return;
+        }
+    }
+}
+const getAllCustomer=()=>{
+    let tbody =$('#t-body');
+    $.each(cusArr,function(i,item){
+        let row = $('<tr>');
+        let colName = $('<td>').text(item,name);
+        let colAddress = $('<td>').text(item,address);
+        let colSalary = $('<td>').text(item,salary);
+        let colnic = $('<td>').text(item,nic);
+
+        row.append(colName,colAddress,colSalary,colSalary);
+        tbody.append(row);
+    })
+}
 
 const clearAndLoad = (element)=>{}
 
